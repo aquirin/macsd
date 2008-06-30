143bcfd4d9607ba2803a0afab47cfd958e574534
kitutou <kitutou@0822759f-e250-0410-87c2-372e7d591b83>
2008-06-30 16:11:30 +0000

    fixed by Rocio

--- trunk/src-macsd/shapes.h
+++ trunk/src-macsd/shapes.h
@@ -10,16 +10,15 @@
 #include <fstream>
 #include "utils.h"
 #include <iostream>
-#include "routines.h"
 
 using namespace std;
 
 class shapes {
     public:
-        shapes() {_nodo = -1; _num_nodes = -1; _num_edges = -1; };
-        shapes(const unsigned int x, const unsigned int l);
-        ~shapes();
-        shapes(const shapes& s);
+        shapes() {_nodo = -1;};
+        shapes(const unsigned int x, const unsigned int l) : _grafo(x, vector<vector <bool> >(x, vector<bool>(l, false))) {_nodo = -1;};
+        ~shapes() {};
+        shapes(const shapes& s) : _grafo(s._grafo) {_nodo = s._nodo;};
         
         void agregarEje(const unsigned int ini, const unsigned int fin, const unsigned int s);
         void agregarNodo(const int nod) {assert((nod-1 < cantNodos()) && (_nodo == -1)); _nodo = nod-1;};
@@ -27,7 +26,7 @@ class shapes {
                 
         unsigned int size() const;
         
-        bool ejeUsado(const unsigned int ini, const unsigned int fin, const unsigned int s) const;
+        bool ejeUsado(const unsigned int ini, const unsigned int fin, const unsigned int s) const {return _grafo[ini-1][fin-1][s-1];};
         
         bool nodoUsado(const unsigned int nod) const;
         
@@ -37,25 +36,17 @@ class shapes {
         
         unsigned int elNodo() const {return _nodo;};
         
-        unsigned int cantNodos() const {return _num_nodes;};
-        unsigned int cantEjes() const {return _num_edges;};
+        unsigned int cantNodos() const {return _grafo.size();};
+        unsigned int cantEjes() const {return _grafo[0][0].size();};
 
         void imprime(ostream &salida) const;
         
         bool operator== (const shapes& s) const;
         
         bool operator!= (const shapes& s) const {return !(*this==s);};
-	
-        /**
-        operador de asignacion
-        */
-        shapes & operator= (const shapes & aShape);
-	
     protected:
         int _nodo;
-	int _num_nodes, _num_edges;
-	set< unsigned int > _nodes;
-	set<tuplax3< unsigned int> > _edges;
+        vector< vector< vector<bool> > > _grafo;
 };
 
 ostream& operator<<(ostream& os, const shapes& s);
